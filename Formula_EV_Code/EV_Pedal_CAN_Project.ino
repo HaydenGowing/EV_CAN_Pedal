e5befe3284a2501ff7d902da1c74e1738e5f6ec7
HaydenGowing <97248426+HaydenGowing@users.noreply.github.com>
2023-08-01 10:45:51 -0600

    Update EV_Pedal_CAN_Project.ino

--- Formula_EV_Code/EV_Pedal_CAN_Project.ino
+++ Formula_EV_Code/EV_Pedal_CAN_Project.ino
@@ -6,36 +6,55 @@ void setup() {
   while (!Serial);
   Serial.println("Pedal voltage to CAN message program");
 
-  // start the CAN bus at 500 kbps
-  if (!CAN.begin(500E3)) {
-    
+  if (!CAN.begin(500E3)) // We have 4 different bit rate: 1) 125 kbps, 250kbps, 500kbps, 1Mbps
+  {
     Serial.println("Starting CAN failed!");
     while (1);
   }
 }
 
-int brake_pedal_pin = A0;
-int accelerator_pedal_pin = A1;
+//Start range voltage is 1.4/4.4 V
+//End range voltage is 1/4.8 V
+// Plotted in desmos: https://www.desmos.com/calculator/7ob8hfbr4l
+
+
+// APPS_1 Working voltage range: 1.4 - 4.8
+// APPS_2 Working voltage range: 4.4 - 1
+
+int APPS_1_Pin = A0;
+int APPS_2_pin = A1;
 int status;
+
+float tolerance = 0.1 //Voltage tolerance between pedal voltages
 float ADC_conversion = 5/1023;
+
+
+// Accounts for the jitter/error when switching channels on ADC multiplexer
+void adc_read_compensation(int reading, int pin) 
+{
+  reading = analogRead(pin) //Switches ADC mux channels
+  delay(10)
+  reading = analogRead(pin) //Gets a more stable reading after switching
+  delay(10)
+
+  return reading 
+}
+
+
 
 void signal_pedal_verification(int pedal_1, int pedal_2) 
 {
-  brake_reading = analogRead(pedal_1);
-  accelerator_reading = analogRead(pedal_2);
+  adc_read_compensation(APPS_1, APPS_1_pin)
+  adc_read_compensation(APPS_2, APPS_2_pin)
 
-  brake_voltage = float(brake_pedal_pin * ADC_conversion);
-  accelerator_voltage = float(accelerator_pedal_pin * ADC_conversion);
+  APPS_1_Voltage = float(APPS_1 * ADC_conversion);
+  APPS_2_Voltage = float(APPS_2 * ADC_conversion);
 
-  
-
- voltage_conditions = [brake_voltage > 2.5 && accelerator < 2.5, brake_voltage < 2.5 && accelerator > 2.5, brake_voltage && accelerator == 2.5];
-
-  //Assuming brake_voltage = 5 V at rest so accelerator_voltage = 0 V at rest
- if( 1 // Maybe we cycle through the conditions, or we could store previous couple pedal outputs and make sure multiple signals are correct vs just one) 
+// Have to account for the 0.8V starting voltage for APPS1
+ if(5 - (APPS_2_Voltage + tolerance) < APPS_1_Voltage - 0.8 < 5 - (APPS_2_Voltage - tolerance))
  {
    status = 1; 
- }
+  }
  else
  {
    status = 0;
@@ -43,25 +62,26 @@ void signal_pedal_verification(int pedal_1, int pedal_2)
   return status
 }
 
+// Map out voltages to angles
 void loop() {
 
   pinmode(brake_pedal_pin, INPUT);
   pinmode(accelerator_pedal_pin, INPUT);
 
-  // send packet: id is 11 bits, packet can contain up to 8 bytes of data 
   signal_pedal_verification(brake_pedal_pin, accelerator_pedal_pin);
 
 ////// Probably will turn these into functions depending on complexity of content in 'if' loops
   if (status == 0) // Pedals Fail
   {
-
+    
   }
-
   if (status == 1) // Pedals verified
   {
     
   }
 //////
+
+// send packet: id is 11 bits, packet can contain up to 8 bytes of data 
   Serial.print("Sending packet ... ");
 
   CAN.beginPacket(0x12);
@@ -69,7 +89,6 @@ void loop() {
   CAN.endPacket();
 
   Serial.println("done");
-
   delay(1000);
 
   // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
@@ -80,7 +99,6 @@ void loop() {
   CAN.endPacket();
 
   Serial.println("done");
-
   delay(1000);
 }
 
